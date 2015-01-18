3daceaf9588e2f97445483a9f997439233b53669
Steven Kauyedauty <skauyedauty@gmail.com>
2015-01-17 21:47:49 -0700

    simple loop review

---
+++ main.js
@@ -0,0 +1,42 @@
+$(document).on('ready', function() {
+  
+});
+// loop #1
+// for(var i=0; i<=500; i+=100) {
+// 	console.log("here is loop #1: ");
+// 	console.log(i);
+// }
+// // loop#1
+// for(var i =1; i<=64; i*=2) {
+// 	console.log(i);
+// }
+
+// for(var i =1; i<=3;i++) {
+// 	console.log(i);
+// 	console.log(i);
+// 	console.log(i);
+// }
+
+
+// console.log('1,1,1, 2,2,2,3,3,3');
+// for(var i =1; i<=3; i++) {
+// 	for(var x =1; x<=3; x++) {
+// 		console.log(i);
+// 	}
+// }
+
+// for(var i=0; i<=10; i+=2) {
+// 	console.log(i);
+// }
+
+// for(var i=9; i>=0; i--) {
+// 	console.log(i);
+// }
+console.log("expected outcome for #7: 01231230123");
+for(var i=0; i<3;i++) {
+	// console.log(i);
+	for(var x=0; x<=3;x++) {
+		console.log(x);
+		// console.log("here is i:"+i);
+	}
+}
